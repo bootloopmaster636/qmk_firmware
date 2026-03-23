a194c4a4883aab776c278d52b3f8c6a26eabf41e
bootloopmaster636 <christhartono@hotmail.com>
2026-03-23 11:32:49 +0700

    tidy up oled folder

--- keyboards/kbdksp/geulis/rules.mk
+++ keyboards/kbdksp/geulis/rules.mk
@@ -2,12 +2,14 @@ SERIAL_DRIVER = vendor
 
 OLED_DRIVER = ssd1306
 
-SRC += oled/oled_handler.c process_record.c
+OLED_DIR = oled
+OLED_FILES = oled_handler.c \
+             rp2040/master/oled_master_handler.c \
+             rp2040/slave/oled_slave_handler.c
+
+SRC += $(addprefix $(OLED_DIR)/, $(OLED_FILES)) process_record.c
 
 DEBUG_MATRIX_SCAN_RATE_ENABLE = yes
-
-SRC +=  oled/rp2040/master/oled_master_handler.c \
-				oled/rp2040/slave/oled_slave_handler.c
 
 RGBLIGHT_ENABLE = no
 RGBLIGHT_DRIVER = ws2812
