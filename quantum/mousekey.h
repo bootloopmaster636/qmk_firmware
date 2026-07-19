b07c3ee7724dec629b8244e6ad2e8d13879c5f97
bootloopmaster636 <christhartono@hotmail.com>
2026-07-19 07:24:03 +0700

    Revert "Merge branch 'feature/mousekeys-ratio' into kbdksp-bootloopmaster636-mod"
    This reverts commit 1d21d7f324b830a79081e9f679c02a382cfee75c, reversing
    changes made to 1db248f65b8ffc6a3e837f3295480f0436050728.

--- quantum/mousekey.h
+++ quantum/mousekey.h
@@ -76,11 +76,8 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #            define MOUSEKEY_TIME_TO_MAX 30
 #        endif
 #    endif
-#    ifndef MOUSEKEY_SENS_X
-#        define MOUSEKEY_SENS_X 1.0
-#    endif
-#    ifndef MOUSEKEY_SENS_Y
-#        define MOUSEKEY_SENS_Y 1.0
+#    ifndef MOUSEKEY_RATIO
+#        define MOUSEKEY_RATIO 1.0
 #    endif
 #    ifndef MOUSEKEY_WHEEL_DELAY
 #        define MOUSEKEY_WHEEL_DELAY 10
