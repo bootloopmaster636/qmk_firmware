6339726e6e187e11375cc3cf67f463cd192ed701
bootloopmaster636 <christhartono@hotmail.com>
2026-07-28 20:30:51 +0700

    change some config

--- keyboards/kbdksp/geulis/keymaps/bootloopmaster636/keymap.c
+++ keyboards/kbdksp/geulis/keymaps/bootloopmaster636/keymap.c
@@ -91,7 +91,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_NO  ,QK_MOUSE_BUTTON_3,    QK_MOUSE_BUTTON_2,      QK_MOUSE_CURSOR_UP,     QK_MOUSE_BUTTON_1,     KC_NO,                                     KC_NO,                  KC_NO,              KC_NO,                  KC_NO,                  KC_NO, KC_NO,
     KC_NO  ,  KC_NO,    QK_MOUSE_CURSOR_LEFT,   QK_MOUSE_CURSOR_DOWN,   QK_MOUSE_CURSOR_RIGHT,  KC_NO, KC_NO,                     KC_NO,            KC_LEFT,                KC_DOWN,            KC_UP,                  KC_RIGHT,               KC_NO, KC_NO,
     KC_TRNS,  KC_NO,    KC_NO,                  QK_MOUSE_BUTTON_4,      QK_MOUSE_BUTTON_5,      KC_NO, KC_NO, KC_NO,              KC_NO, KC_NO,     QK_MOUSE_WHEEL_RIGHT,   QK_MOUSE_WHEEL_UP,  QK_MOUSE_WHEEL_DOWN,    QK_MOUSE_WHEEL_LEFT,    KC_NO, KC_NO,
-    KC_TRNS,  KC_TRNS,  KC_NO,                  KC_TRNS,                QK_MOUSE_BUTTON_1,                                                          KC_RSFT,                KC_NO,              KC_NO,                  KC_NO,                  KC_NO
+    KC_TRNS,  KC_TRNS,  KC_NO,                  KC_TRNS,                QK_MOUSE_BUTTON_1,                                                          KC_RSFT,                KC_TRNS,              KC_NO,                  KC_NO,                  KC_NO
 ),
 
 
@@ -102,7 +102,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_F1    ,  KC_F2,      KC_F3,  KC_F4,  KC_F5,  KC_F6,                                                                  KC_F7,      KC_F8,      KC_F9,      KC_F10, KC_F11,     KC_F12,
     KC_GRAVE ,  KC_1,       KC_2,   KC_3,   KC_4,   KC_5,   KC_TRNS,                                            KC_TRNS,    KC_6,       KC_7,       KC_8,       KC_9,   KC_0,       KC_NO,
     KC_TRNS  ,  KC_NO,      KC_PRINT_SCREEN,KC_NO,  KC_INSERT, KC_DELETE, KC_TRNS, KC_TRNS,         KC_TRNS,    KC_TRNS,    KC_NO,      KC_HOME,    KC_END,     KC_DOT,  KC_NO,      KC_NO,
-    KC_TRNS  ,  KC_TRNS,    KC_TRNS,KC_NO,  KC_TRNS,                                                                        KC_TRNS,    KC_TRNS,    KC_TRNS,    KC_NO,   KC_NO
+    KC_TRNS  ,  KC_TRNS,    KC_TRNS,KC_TRNS,  KC_TRNS,                                                                        KC_TRNS,    KC_TRNS,    KC_TRNS,    KC_NO,   KC_NO
 ),
 
 
