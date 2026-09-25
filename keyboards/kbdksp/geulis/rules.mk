69d50539666df74a1b0c08dca229789988aa586d
bootloopmaster636 <christhartono@hotmail.com>
2026-09-25 09:10:41 +0700

    Revert "feat: use orbital mouse from getreuer"
    This reverts commit f7adaa3f70cbe77a98a4085fe28c0b2a8ec162e5.

--- keyboards/kbdksp/geulis/rules.mk
+++ keyboards/kbdksp/geulis/rules.mk
@@ -30,7 +30,7 @@ MOUSEKEY_ENABLE = yes
 
 # Other
 COMBO_ENABLE = yes
-DEBOUNCE_TYPE = sym_eager_pr
+DEBOUNCE_TYPE = sym_eager_pk
+NKRO_ENABLE = yes
 UNICODE_COMMON = yes
 OS_DETECTION_ENABLE = yes
-KEY_LOCK_ENABLE = yes
